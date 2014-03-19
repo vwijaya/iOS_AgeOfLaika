118358bde297b4f79244182097dc971bbb95aa2e
Valerino Wijaya <valerino@gmail.com>
2014-03-19 15:57:15 +0700

    Initial Commit

---
+++ AgeOfLaikaTests/AgeOfLaikaTests.m
@@ -0,0 +1,34 @@
+//
+//  AgeOfLaikaTests.m
+//  AgeOfLaikaTests
+//
+//  Created by Valerino on 3/19/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AgeOfLaikaTests : XCTestCase
+
+@end
+
+@implementation AgeOfLaikaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
